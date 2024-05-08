8729af4d934ac7d785d17f22ab33f11bca12637b
henninggk <166753672+henninggk@users.noreply.github.com>
2024-05-08 11:51:05 +0200

    Update sketch-5.js

--- sketch-5.js
+++ sketch-5.js
@@ -1,23 +1,29 @@
-let avstand = 3
-let antall = 80
+let avstand = 3;
+let antall = 80;
+let scaleFactor;
 
 function setup(){
-  createCanvas(800, 800)
-  frameRate(10)
+  createCanvas(windowWidth, windowHeight);
+  frameRate(10);
+  scaleFactor = width / 1600; // Skaleringsfaktor basert på standard canvas bredde (800)
 }
 
 function draw() {
   fill(0);
   noStroke();
-  rectMode(CENTER)
-  translate(width/2, height/2)
+  rectMode(CENTER);
+  translate(width/2, height/2);
  
-  push()
-  clear()
-  for (let i=0; i<antall; i++){
-    
-    rect(i*avstand, 10, 35, 35);
-    rotate(radians(10*mouseX/10));
+  push();
+  clear();
+  for (let i = 0; i < antall; i++){
+    rect(i * avstand * scaleFactor, 10 * scaleFactor, 35 * scaleFactor, 35 * scaleFactor);
+    rotate(radians(10 * mouseX / 10));
   }
-  pop()
+  pop();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  scaleFactor = width / 800; // Oppdaterer skaleringsfaktoren når vindustørrelsen endres
 }
